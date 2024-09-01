71e8f1ec6933bde2a7ee39ad64f58161f84f41cd
nitindpatil15 <nitindpatil2002@gmail.com>
2024-09-01 22:16:41 +0530

    Woek done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cors from "cors";
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://master--movie-ticket-booking-ur.netlify.app/",
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   headers: ["Content-Type", 'Authorization', 'auth-token'],
   credentials: true,
